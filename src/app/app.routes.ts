0ec6946cb9d6c95f5214c17a5d7600e15422c3ba
Coco10023 <mauricio2003@live.se>
2026-05-01 23:19:05 +0200

    Lägger till components för home, converter, routing och about.
    Konfigurerar Angular routing mellan sidorna.
    
    Lägger till navbar på app.html

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,10 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
-import { ConverterComponent } from './pages/converter/converter.component';
-import { AboutComponent } from './pages/about/about.component';
+import { Home } from './pages/home/home'; 
+import { Converter } from './pages/converter/converter'; 
+import { About } from './pages/about/about'; 
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'converter', component: ConverterComponent },
-  { path: 'about', component: AboutComponent },
-  { path: '**', redirectTo: '' }
+  { path: '', component: Home },
+  { path: 'converter', component: Converter },
+  { path: 'about', component: About }
 ];
