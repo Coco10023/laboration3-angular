84bd8611be6d7c52b6fab94c1e537bc416c76293
Coco10023 <mauricio2003@live.se>
2026-05-02 10:29:17 +0200

    Bygger upp min hemsida och implementerar enhets konverterare med data bindning

--- src/app/pages/converter/converter.ts
+++ src/app/pages/converter/converter.ts
@@ -1,9 +1,32 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-converter',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule],
   templateUrl: './converter.html',
-  styleUrl: './converter.css',
+  styleUrl: './converter.css'
 })
-export class Converter {}
+export class Converter {
+  meters = 0;
+  feet = 0;
+  celsius = 0;
+  fahrenheit = 32;
+
+  metersToFeet() {
+    this.feet = +(this.meters * 3.28084).toFixed(2);
+  }
+
+  feetToMeters() {
+    this.meters = +(this.feet / 3.28084).toFixed(2);
+  }
+
+  celsiusToFahrenheit() {
+    this.fahrenheit = +((this.celsius * 9) / 5 + 32).toFixed(2);
+  }
+
+  fahrenheitToCelsius() {
+    this.celsius = +(((this.fahrenheit - 32) * 5) / 9).toFixed(2);
+  }
+}
